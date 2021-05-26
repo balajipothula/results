1eea69be4b3fa6bc91cccf754bb048263f2cf2bd
BALAJI POTHULA <balan.pothula@gmail.com>
2021-05-26 14:14:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,19 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-south-1"
- #profile = "nginxprofile"
 
+  region  = "ap-south-1"
+
+  access_key = "your_access_key"
+  secret_key = "your_secret_key"
+
+//profile = "terraform"
+
+/*
   assume_role {
-    role_arn = "arn:aws:iam::752042071855:role/EC2FullAccess"
+    role_arn = "arn:aws:iam::123456789012:role/terraform"
   }
+*/
 
   default_tags {
 
@@ -59,7 +66,7 @@ variable "availability_zones" {
 
 variable "instance_count" {
   type        = number
-  default     = 2
+  default     = 1
   description = "Instance Count"
 }
 
@@ -81,7 +88,7 @@ variable "vpc_security_group_ids" {
   description = "Security Group IDs"
 }
 
-resource "aws_instance" "nginx_server" {
+resource "aws_instance" "platform" {
   ami                    = lookup(var.ami, var.aws_region)                   # Amazon Machine Image.
   availability_zone      = "${element(var.availability_zones, count.index)}" # Availability Zone Name.
   count                  = var.instance_count                                # Number of Instances.
@@ -92,22 +99,25 @@ resource "aws_instance" "nginx_server" {
   user_data = <<-USER_DATA
     #!/bin/bash
     sudo apt -y update
-    sudo apt -y install nginx
+    sudo apt -y install nginx software-properties-common
+    sudo add-apt-repository -y ppa:deadsnakes/ppa
+    sudo apt -y update
+    sudo apt -y install python3.8
     sudo service nginx start
   USER_DATA
 
   tags = {
-    Name = "nginx-server-${count.index}"
+    Name = "platform-${count.index}"
   }
 
 }
 
 output "instance_id" {
-  value       = aws_instance.nginx_server.*.id
+  value       = aws_instance.platform.*.id
   description = "AWS Instance IDs"
 }
 
 output "instance_public_ip" {
-  value       = aws_instance.nginx_server.*.public_ip
+  value       = aws_instance.platform.*.public_ip
   description = "AWS Instance Public IPs"
 }
